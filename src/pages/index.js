43141e833a5caef6214e459f5808ac8d4a846913
zhuayu <294242589@qq.com>
2017-12-20 20:29:32 +0800

    DEV ENV

---
+++ src/pages/index.js
@@ -0,0 +1,55 @@
+Page({
+    data: {
+        list: [
+            {
+                id: 'z-index',
+                name: '全局',
+                open: false,
+                pages: ['color', 'font']
+            },
+            {
+                id: 'form',
+                name: '表单',
+                open: false,
+                // pages: ['button', 'list', 'input', 'slider', 'uploader']
+            },
+            {
+                id: 'widget',
+                name: '基础组件',
+                open: false,
+                // pages: ['article', 'badge', 'flex', 'footer', 'gallery', 'grid', 'icons', 'loadmore', 'panel', 'preview', 'progress']
+            },
+            {
+                id: 'feedback',
+                name: '操作反馈',
+                open: false,
+                // pages: ['actionsheet', 'dialog', 'msg', 'picker', 'toast']
+            },
+            {
+                id: 'nav',
+                name: '导航相关',
+                open: false,
+                // pages: ['navbar', 'tabbar']
+            },
+            {
+                id: 'search',
+                name: '搜索相关',
+                open: false,
+                // pages: ['searchbar']
+            }
+        ]
+    },
+    kindToggle: function (e) {
+        var id = e.currentTarget.id, list = this.data.list;
+
+        list.forEach((data,index)=>{
+            if(data.id == id){
+                data.open = !data.open
+            }
+        })
+
+        this.setData({
+            list: list
+        });
+    }
+});
