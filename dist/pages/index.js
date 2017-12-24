4e1562b63bd4a8cad5e629bb28345d0479c42dcc
zhuayu <294242589@qq.com>
2017-12-24 13:07:08 +0800

    Add#button list input

--- dist/pages/index.js
+++ dist/pages/index.js
@@ -12,6 +12,7 @@ Page({
                 name: '表单',
                 open: false,
                 // pages: ['button', 'list', 'input', 'slider', 'uploader']
+                pages: ['button','list', 'input']
             },
             {
                 id: 'widget',
